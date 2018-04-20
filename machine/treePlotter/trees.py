5390f9ec8e47238d41ceba7453db142300945be1
1756983926@qq.com <1756983926@qq.com>
2018-04-20 18:55:03 +0800

    add files

---
+++ machine/treePlotter/trees.py
@@ -0,0 +1,47 @@
+# !/usr/bin/python
+# coding:utf-8
+# ***************************************************************
+# 决策树
+# author:   pruce
+# email:    1756983926@qq.com
+# date:     2018年4月19日
+# ***************************************************************
+
+from math import log
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)
+
+    return shannonEnt
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[: axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing','flippers']
+    return dataSet, labels
+
+dataSet, labels = createDataSet()
+print calcShannonEnt(dataSet)
