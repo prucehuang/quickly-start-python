1e60899473a2057c533aad9f8743ad1fb126ab35
Pruce <1756983926@qq.com>
2018-12-15 19:52:35 +0800

    modify ML files from PC

---
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/02_end_to_end_machine_learning_project.py
@@ -0,0 +1,13 @@
+# !usr/bin/env python
+# coding:utf-8
+
+"""
+
+author: prucehuang
+@email: 1756983926@qq.com
+@modify:
+@time: 2018/12/15
+"""
+
+if __name__ == "__main__":
+    print('Hello, Welcome to My World')
