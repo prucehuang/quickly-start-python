0ecc15d8145d3a4fa0b2d648811884e1b5522e12
prucehuang(黄海峰) <prucehuang@tencent.com>
2018-12-18 07:54:47 +0800

    add files

---
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/03_classification.py
@@ -0,0 +1,34 @@
+# !usr/bin/env python
+# coding:utf-8
+
+"""
+分类
+author: prucehuang
+@email: 1756983926@qq.com
+@modify:
+@time: 2018/12/18
+"""
+import pandas as pd
+pd.set_option('display.width', 1000)  # 设置字符显示宽度
+pd.set_option('display.max_columns', None) # 打印所有列，类似的max_rows打印所有行
+
+if __name__ == "__main__":    
+    print('Hello world')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
