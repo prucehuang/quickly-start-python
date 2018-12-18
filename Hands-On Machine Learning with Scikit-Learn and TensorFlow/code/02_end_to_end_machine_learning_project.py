7fe882e678b45b3091d2f793e8ef0fb65905c2eb
Pruce <1756983926@qq.com>
2018-12-18 20:39:07 +0800

    add pycharm setting from PC

--- Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/02_end_to_end_machine_learning_project.py
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/02_end_to_end_machine_learning_project.py
@@ -17,8 +17,9 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
-from sklearn.pipeline import FeatureUnion
+from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 
 # file path
 PROJECT_ROOT_DIR = sys.path[0] + '/../'
@@ -128,10 +129,13 @@ def discover_visualize_data():
     housing.plot(kind="scatter", x="longitude_latitude", y="median_house_value", alpha=0.1)
     plot.show()
 
-# axis=1, reduce the columns, return a Series whose index is the original index 返回所有列不为空的index
-# 与axis=1 对应的是 axis=0，reduce the index, return a Series whose index is the original column labels 返回所有数据不为空的列名
-# 与any() 之对应的是 all()， any表示任意一个True则返回True，all表示任意一个为False则返回False
-get_no_null_data = lambda df: df[df.isnull().any(axis=1)]
+def get_no_null_data(df):
+    '''
+        axis=1, reduce the columns, return a Series whose index is the original index 返回所有列不为空的index
+        与axis=1 对应的是 axis=0，reduce the index, return a Series whose index is the original column labels 返回所有数据不为空的列名
+        与any() 之对应的是 all()， any表示任意一个True则返回True，all表示任意一个为False则返回False
+    '''
+    return df[df.isnull().any(axis=1)]
 
 class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
     def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
@@ -144,7 +148,7 @@ class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
 
         rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
         population_per_household = X[:, population_ix] / X[:, household_ix]
-        longitude_latitude = X[:, longitude_ix] / X[:, latitude_ix]
+        longitude_latitude = X[:, longitude_ix] + X[:, latitude_ix]
         if self.add_bedrooms_per_room:
             bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
             return np.c_[X, rooms_per_household, population_per_household, longitude_latitude,
@@ -152,9 +156,74 @@ class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
         else:
             return np.c_[X, rooms_per_household, population_per_household, longitude_latitude]
 
+class CustomLabelBinarizer(BaseEstimator, TransformerMixin):
+    def __init__(self, sparse_output = False):
+        self.sparse_output = sparse_output
+    def fit(self, X, y = None):
+        return self
+    def transform(self, X, y = None):
+        enc = LabelBinarizer(sparse_output = self.sparse_output)
+        return enc.fit_transform(X)
+
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        return X[self.attribute_names].values
+
+def feature_clear_prepare():
+    ## NULL值处理 用中位数填补
+    print(get_no_null_data(housing).head()) # 查看有空值的特征数据
+    imputer = SimpleImputer(strategy="median")  # 空值用中位数替换
+    housing_num = housing.select_dtypes(include=[np.number])
+    imputer.fit(housing_num)
+    print(imputer.statistics_) # equal print(housing_num.median().values)
+    X = imputer.transform(housing_num)
+    housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=list(housing.index.values))
+    print(housing_tr.head())
+    print(housing_labels.head())
+
+    ## 字符串数值处理
+    housing_cat = housing[['ocean_proximity']]
+    print(housing_cat.head(10))
+    housing_cat_1hot = LabelBinarizer().fit_transform(housing_cat)
+    print(housing_cat_1hot)
+
+    ## 定义组合特征
+    attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+    housing_extra_attribs = attr_adder.transform(housing.values)
+    housing_extra_attribs = pd.DataFrame(
+        housing_extra_attribs,
+        columns=list(housing.columns) + ['rooms_per_household', 'population_per_household', 'longitude_latitude'])
+    print(housing_extra_attribs.head())
+
+def feature_clear(housing):
+    housing_num = housing.select_dtypes(include=[np.number])
+    num_attribs = list(housing_num)
+    cat_attribs = ['ocean_proximity']
+
+    num_pipeline = Pipeline([
+        ('selector', DataFrameSelector(num_attribs)),
+        ('imputer', SimpleImputer(strategy="median")),
+        ('attribs_adder', CombinedAttributesAdder()),
+        ('std_scaler', StandardScaler()),
+    ])
+    cat_pipeline = Pipeline([
+        ('selector', DataFrameSelector(cat_attribs)),
+        ('label_binarizer', CustomLabelBinarizer()),
+    ])
+    full_pipeline = ColumnTransformer([
+        ("num_pipeline", num_pipeline, num_attribs),
+        ("cat_pipeline", cat_pipeline, cat_attribs),
+    ])
+    return full_pipeline.fit_transform(housing)
+
 if __name__ == "__main__":
     pd.set_option('display.width', 1000)  # 设置字符显示宽度
     pd.set_option('display.max_columns', None) # 打印所有列，类似的max_rows打印所有行
+
     '''
         加载数据，并将数据划分成训练数据、测试数据
     '''
@@ -171,62 +240,32 @@ if __name__ == "__main__":
     print('train set count', train_set_size, ', percent', train_set_size*1.0/(train_set_size+test_set_size),
           '\ntest set count',test_set_size, ', percent', test_set_size*1.0/(train_set_size+test_set_size), '\n')
     housing = train_set.copy()
-    
+    housing_labels = housing["median_house_value"].copy()
+    housing = housing.drop("median_house_value", axis=1)  # drop labels for training set
+
     '''
         探索特征之间的相关性
     '''
-    housing_labels = housing["median_house_value"].copy()
-    housing = housing.drop("median_house_value", axis=1)  # drop labels for training set
     # discover_visualize_data()
     
     '''
         特征处理
     '''
-    ## NULL值处理
-    # print(get_no_null_data(housing).head()) # 查看有空值的特征数据
-    imputer = SimpleImputer(strategy="median") # 空值用中位数替换
-    housing_num = housing.select_dtypes(include=[np.number])
-    imputer.fit(housing_num)
-    # print(imputer.statistics_) # equal print(housing_num.median().values)
-    X = imputer.transform(housing_num)
-    housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=list(housing.index.values))
-    # print(housing_tr.head())
-    # print(housing_labels.head())
+    # feature_clear_prepare()
+    housing_prepared = feature_clear(housing)
 
-    ## 字符串数值处理
-    housing_cat = housing[['ocean_proximity']]
-    print(housing_cat.head(10))
-    housing_cat_1hot = LabelBinarizer().fit_transform(housing_cat)
-    print(housing_cat_1hot)
-    
-    
-    ##
-    
-    
-    
-    attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
-    housing_extra_attribs = attr_adder.transform(housing.values)
-    housing_extra_attribs = pd.DataFrame(
-            housing_extra_attribs,
-            columns=list(housing.columns)+['rooms_per_household', 'population_per_household', 'longitude_latitude'])
-    print(housing_extra_attribs.head())
-    
-    num_pipeline = Pipeline([
-        ('imputer', SimpleImputer(strategy="median")),
-        ('attribs_adder', CombinedAttributesAdder()),
-        ('std_scaler', StandardScaler()),
-    ])
+    '''
+        算法模型
+    '''
+    lin_reg = LinearRegression()
+    lin_reg.fit(housing_prepared, housing_labels)
 
-    # print(num_pipeline.fit_transform(housing_num).head())
-    num_attribs = list(housing_num)
-    cat_attribs = ["ocean_proximity"]
+    # let's try the full preprocessing pipeline on a few training instances
+    some_data = housing_prepared[:10]
+    some_labels = housing_labels[:10]
+    print(some_data.shape)
 
-    full_pipeline = FeatureUnion([
-        ("num", num_pipeline, num_attribs),
-        ("cat", LabelBinarizer(), cat_attribs),
-    ])
-
-    #housing_prepared = full_pipeline.fit_transform(housing)
+    print("Predictions:", lin_reg.predict(some_data))
     
     
     
