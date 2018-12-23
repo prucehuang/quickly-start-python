22be6f60b237f9576634b1f6c21a0bc1e5a2f2ca
Pruce <1756983926@qq.com>
2018-12-23 19:50:30 +0800

    add ML files from PC

--- Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/03_classification.py
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/03_classification.py
@@ -9,12 +9,24 @@ author: prucehuang
 @time: 2018/12/18
 """
 import pandas as pd
-pd.set_option('display.width', 1000)  # 设置字符显示宽度
-pd.set_option('display.max_columns', None) # 打印所有列，类似的max_rows打印所有行
+from sklearn.datasets import load_digits
+import matplotlib
+import matplotlib.pyplot as plt
 
-if __name__ == "__main__":    
-    print('Hello world')
-    
+if __name__ == "__main__":
+    pd.set_option('display.width', 1000)  # 设置字符显示宽度
+    pd.set_option('display.max_columns', None)  # 打印所有列，类似的max_rows打印所有行
+    mnist = load_digits()
+    # data, target, target_names, images, descr
+    X, y = mnist["data"], mnist["target"]
+    print(X.shape, y.shape)
+    some_digit = X[30]
+    some_digit_image = some_digit.reshape(8, 8)
+    plt.imshow(some_digit_image, cmap=matplotlib.cm.binary,
+               interpolation="nearest")
+    plt.axis("off")
+    plt.show()
+
     
     
     
