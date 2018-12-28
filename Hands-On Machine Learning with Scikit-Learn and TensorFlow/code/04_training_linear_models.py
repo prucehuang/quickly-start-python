bc2e0e0424d86153c35d5ea832d996c73d680b52
1756983926@qq.com <1756983926@qq.com>
2018-12-28 20:26:49 +0800

    add ML files from PC

--- Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/04_training_linear_models.py
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/04_training_linear_models.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 
 """
-
+训练线性模型
 author: prucehuang 
  email: 1756983926@qq.com
   date: 2018/12/27
@@ -14,21 +14,17 @@ from sklearn.linear_model import LinearRegression
 X = 2 * np.random.rand(100, 1)
 y = 4 + 3 * X + np.random.randn(100, 1)
 
-if __name__ == "__main__":
-    plt.plot(X, y, "b.")
-    plt.xlabel("$x_1$", fontsize=18)
-    plt.ylabel("$y$", rotation=0, fontsize=18)
-    plt.axis([0, 2, 0, 15])
-
-    # 用正规化分发直接求解最优theta
+# 正规方程求解
+def normal_equation():
+    # 方式一，使用numpy函数
     X_b = np.c_[np.ones((100, 1)), X]  # add x0 = 1 to each instance
-    theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
-    print(theta_best)
-
+    theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)  # 上公式
+    print(theta_best)  # [[3.95336514] [3.07080187]]
+    # 方式二，使用sklearn函数
     lin_reg = LinearRegression()
     lin_reg.fit(X, y)
-    print(lin_reg.intercept_, lin_reg.coef_)
-
+    print(lin_reg.intercept_, lin_reg.coef_)  # [3.95336514] [[3.07080187]]
+    # 预测两个点 并将这两个点连成线段画出来
     X_new = np.array([[0], [2]])
     X_new_b = np.c_[np.ones((2, 1)), X_new]  # add x0 = 1 to each instance
     y_predict = X_new_b.dot(theta_best)
@@ -36,3 +32,14 @@ if __name__ == "__main__":
     plt.plot(X_new, y_predict, "r-", linewidth=2, label="Predictions")
     plt.legend(loc="upper left", fontsize=14)
     plt.show()
+
+if __name__ == "__main__":
+    plt.plot(X, y, "b.")
+    plt.xlabel("$x_1$", fontsize=18)
+    plt.ylabel("$y$", rotation=0, fontsize=18)
+    plt.axis([0, 2, 0, 15])
+
+    '''
+        用正规方程直接求解最优解
+    '''
+    # normal_equation()
