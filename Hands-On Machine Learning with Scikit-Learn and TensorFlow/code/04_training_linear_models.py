3d07b8741744d9882ed33437fea258231f794038
1756983926@qq.com <1756983926@qq.com>
2019-01-02 20:32:20 +0800

    add ML files from PC

--- Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/04_training_linear_models.py
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/04_training_linear_models.py
@@ -9,7 +9,7 @@ author: prucehuang
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.linear_model import LinearRegression, SGDRegressor, Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
@@ -242,4 +242,36 @@ if __name__ == "__main__":
     '''通过学习曲线来判断模型是否过拟合、是否欠拟合、现在模型是什么状态、下一步应该如何处理'''
     # learning_curves()
 
+    '''正则项模型惩罚'''
+    m = 20
+    X = 3 * np.random.rand(m, 1)
+    y = 1 + 0.5 * X + np.random.randn(m, 1) / 1.5
+    X_new = np.linspace(0, 3, 100).reshape(100, 1)
+
+
+    def plot_model(model_class, polynomial, alphas, **model_kargs):
+        for alpha, style in zip(alphas, ("b-", "g--", "r:")):
+            model = model_class(alpha, **model_kargs) if alpha > 0 else LinearRegression()
+            if polynomial:
+                model = Pipeline([
+                    ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+                    ("std_scaler", StandardScaler()),
+                    ("regul_reg", model),
+                ])
+            model.fit(X, y)
+            y_new_regul = model.predict(X_new)
+            lw = 2 if alpha > 0 else 1
+            plt.plot(X_new, y_new_regul, style, linewidth=lw, label=r"$\alpha = {}$".format(alpha))
+        plt.plot(X, y, "b.", linewidth=3)
+        plt.legend(loc="upper left", fontsize=15)
+        plt.xlabel("$x_1$", fontsize=18)
+        plt.axis([0, 3, 0, 4])
+
+    plt.figure(figsize=(8, 4))
+    plt.subplot(121)
+    plot_model(Ridge, polynomial=False, alphas=(0, 10, 100), random_state=42)
+    plt.ylabel("$y$", rotation=0, fontsize=18)
+    plt.subplot(122)
+    plot_model(Ridge, polynomial=True, alphas=(0, 10 ** -5, 1), random_state=42)
+    plt.show()
 
