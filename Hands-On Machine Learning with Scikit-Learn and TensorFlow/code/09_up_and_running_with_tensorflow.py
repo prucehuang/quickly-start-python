19ce0b9f0b9559c522ef6da6ca7233c0b8e0bc5c
1756983926@qq.com <1756983926@qq.com>
2019-01-25 20:16:39 +0800

    add ML files from PC

--- Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/09_up_and_running_with_tensorflow.py
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/09_up_and_running_with_tensorflow.py
@@ -34,13 +34,12 @@ def save_fig(fig_id, tight_layout=True):
         plt.tight_layout()
     plt.savefig(path, format='png', dpi=300)
 
-if __name__ == "__main__":
-    print('Hello, Welcome to My World')
+def create_graph_and_run_in_session():
     x = tf.Variable(3, name="x")
     y = tf.Variable(4, name="y")
     f = x * x * y + y + 2
     # by now, no computation, just creates a computation graph
-    # 第一种写法
+    # 第一种写法，手动创建并close
     sess = tf.Session()
     sess.run(x.initializer)
     sess.run(y.initializer)
@@ -51,5 +50,27 @@ if __name__ == "__main__":
     with tf.Session() as sess:
         x.initializer.run()
         y.initializer.run()
-        result = f.eval() # equivalent to tf.get_default_session().run(f)
+        result = f.eval()  # equivalent to tf.get_default_session().run(f)
         print(result)
+    # 第三种写法，统一初始化，自动close
+    init = tf.global_variables_initializer()  # prepare an init node
+    with tf.Session() as sess:
+        init.run()  # actually initialize all the variables
+        result = f.eval()
+        print(result)
+    # 第四种写法，自动初始化，没有with语法段，但需要手动close
+    sess = tf.InteractiveSession()
+    init.run()
+    result = f.eval()
+    print(result)
+    sess.close()
+
+if __name__ == "__main__":
+    print('Hello, Welcome to My World')
+    x1 = tf.Variable(1) # default graph
+    print(x1.graph is tf.get_default_graph())
+    graph = tf.Graph() # 创建一个新的graph
+    with graph.as_default():
+        x2 = tf.Variable(2)
+    print(x2.graph is graph)
+    print(x2.graph is tf.get_default_graph())
