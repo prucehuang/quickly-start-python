90b1e6b8c3b8fb690ed25a42ed4ad44f2ba27547
prucehuang(黄海峰) <prucehuang@tencent.com>
2019-03-12 09:10:25 +0800

    add files

--- Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/09_up_and_running_with_tensorflow.py
+++ Hands-On Machine Learning with Scikit-Learn and TensorFlow/code/09_up_and_running_with_tensorflow.py
@@ -6,6 +6,8 @@
 author: prucehuang 
  email: 1756983926@qq.com
   date: 2019/01/24
+  https://arxiv.org/pdf/1609.04747.pdf
+  http://www.cnblogs.com/guoyaohua/p/8544835.html
 """
 # Common imports
 import numpy as np
@@ -84,11 +86,8 @@ def graph_manage():
         x2 = tf.Variable(2)
     print('x2 belong tp a new graph', x2.graph is tf.get_default_graph(), x2.graph is graph)
 
-if __name__ == "__main__":
-    # run一个graph
-    # create_graph_and_run_in_session()
-    # graph管理
-    # graph_manage()
+'''使用多种方法计算LR的theta值'''
+def lr_train():
     reset_graph()
     iris = datasets.load_iris()
     m, n = iris.data.shape
@@ -116,15 +115,15 @@ if __name__ == "__main__":
 
     # 特征压缩
     scaler = StandardScaler()
-    scaled_housing_data = scaler.fit_transform(iris.data)
-    scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data]
+    scaled_iris_data = scaler.fit_transform(iris.data)
+    scaled_iris_data_plus_bias = np.c_[np.ones((m, 1)), scaled_iris_data]
 
     # 方法四：使用TensorFlow手动梯度下降
     reset_graph()
     n_epochs = 1000
     learning_rate = 0.01
 
-    X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+    X = tf.constant(scaled_iris_data_plus_bias, dtype=tf.float32, name="X")
     y = tf.constant(iris.target.reshape(-1, 1), dtype=tf.float32, name="y")
     theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
     y_pred = tf.matmul(X, theta, name="predictions")
@@ -136,7 +135,6 @@ if __name__ == "__main__":
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
-
         for epoch in range(n_epochs):
             if epoch % 100 == 0:
                 print("Epoch", epoch, "MSE =", mse.eval())
@@ -150,7 +148,7 @@ if __name__ == "__main__":
     n_epochs = 1000
     learning_rate = 0.01
 
-    X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+    X = tf.constant(scaled_iris_data_plus_bias, dtype=tf.float32, name="X")
     y = tf.constant(iris.target.reshape(-1, 1), dtype=tf.float32, name="y")
     theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
     y_pred = tf.matmul(X, theta, name="predictions")
@@ -162,7 +160,6 @@ if __name__ == "__main__":
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
-
         for epoch in range(n_epochs):
             if epoch % 100 == 0:
                 print("Epoch", epoch, "MSE =", mse.eval())
@@ -171,3 +168,156 @@ if __name__ == "__main__":
         best_theta_1 = theta.eval()
 
     print('tensorflow audodiff gradients:\n', best_theta_1)
+
+    # 方法六：直接使用GradientDescentOptimizer
+    reset_graph()
+    n_epochs = 1000
+    learning_rate = 0.01
+
+    X = tf.constant(scaled_iris_data_plus_bias, dtype=tf.float32, name="X")
+    y = tf.constant(iris.target.reshape(-1, 1), dtype=tf.float32, name="y")
+    theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+    y_pred = tf.matmul(X, theta, name="predictions")
+    error = y_pred - y
+    mse = tf.reduce_mean(tf.square(error), name="mse")
+    # optimizer
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    training_op = optimizer.minimize(mse)
+    # start execution
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for epoch in range(n_epochs):
+            if epoch % 100 == 0:
+                print("Epoch", epoch, "MSE =", mse.eval())
+            sess.run(training_op)
+        best_theta = theta.eval()
+
+    print("tensorflow gradient descent optimizer:\n", best_theta)
+
+    # 方法七：使用MomentumOptimizer
+    reset_graph()
+    n_epochs = 1000
+    learning_rate = 0.01
+
+    X = tf.constant(scaled_iris_data_plus_bias, dtype=tf.float32, name="X")
+    y = tf.constant(iris.target.reshape(-1, 1), dtype=tf.float32, name="y")
+    theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+    y_pred = tf.matmul(X, theta, name="predictions")
+    error = y_pred - y
+    mse = tf.reduce_mean(tf.square(error), name="mse")
+    # 使用MomentumOptimizer
+    optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9)
+    training_op = optimizer.minimize(mse)
+    # start execution
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        for epoch in range(n_epochs):
+            sess.run(training_op)
+            if epoch % 100 == 0:
+                print("Epoch", epoch, "MSE =", mse.eval())
+        best_theta = theta.eval()
+
+    print("tensorflow momentum optimizer:\n", best_theta)
+
+    # 方法八：随机梯度下降，使用placeholder
+    reset_graph()
+    learning_rate = 0.01
+    iris = datasets.load_iris()
+    m, n = iris.data.shape
+    iris_data_plus_bias = np.c_[np.ones((m, 1)), iris.data]
+    n_epochs = 10
+    batch_size = 100
+    n_batches = int(np.ceil(m / batch_size))
+    scaler = StandardScaler()
+    scaled_iris_data = scaler.fit_transform(iris.data)
+    scaled_iris_data_plus_bias = np.c_[np.ones((m, 1)), scaled_iris_data]
+    # placeholder是一个占位符，后续将使用feed_dict往里面添加实际内容
+    X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X")
+    y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+    theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+    y_pred = tf.matmul(X, theta, name="predictions")
+    error = y_pred - y
+    mse = tf.reduce_mean(tf.square(error), name="mse")
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    training_op = optimizer.minimize(mse)
+
+    init = tf.global_variables_initializer()
+
+    def fetch_batch(epoch, batch_index, batch_size):
+        np.random.seed(epoch * n_batches + batch_index)
+        indices = np.random.randint(m, size=batch_size)  
+        X_batch = scaled_iris_data_plus_bias[indices]  
+        y_batch = iris.target.reshape(-1, 1)[indices]  
+        return X_batch, y_batch
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for epoch in range(n_epochs):
+            for batch_index in range(n_batches):
+                X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+                # 每次只喂一个批次的随机数据
+                sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+        best_theta_2 = theta.eval()
+
+    print("tensorflow mini-batch Gradient Descent Optimizer:\n", best_theta_2)
+
+'''将model存入本地文件，checkpoint到本地文件'''
+def save_load_model():
+    reset_graph()
+    n_epochs = 1000
+    learning_rate = 0.01
+    iris = datasets.load_iris()
+    m, n = iris.data.shape
+    scaled_iris_data = StandardScaler().fit_transform(iris.data)
+    scaled_iris_data_plus_bias = np.c_[np.ones((m, 1)), scaled_iris_data]
+
+    X = tf.constant(scaled_iris_data_plus_bias, dtype=tf.float32, name="X")
+    y = tf.constant(iris.target.reshape(-1, 1), dtype=tf.float32, name="y")
+    theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+    y_pred = tf.matmul(X, theta, name="predictions")
+    error = y_pred - y
+    mse = tf.reduce_mean(tf.square(error), name="mse")
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    training_op = optimizer.minimize(mse)
+
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()  # 模型存储对象
+    esave = 'restore' # save|load|restore
+    if esave=='save':
+        with tf.Session() as sess:
+            sess.run(init)
+            for epoch in range(n_epochs):
+                if epoch % 100 == 0:
+                    print("Epoch", epoch, "MSE =", mse.eval())
+                    print(saver.save(sess, "D:/Document/my_model.ckpt"))
+                sess.run(training_op)
+            best_theta = theta.eval()
+            save_path = saver.save(sess, "D:/Document/my_model_final.ckpt")
+            print(save_path, '\n', best_theta)
+    elif esave=='load':
+        with tf.Session() as sess:
+            saver.restore(sess, "D:/Document/my_model_final.ckpt")
+            best_theta_restored = theta.eval()
+            print(best_theta_restored)
+    elif esave=='restore':
+        reset_graph()
+        # notice that we start with an empty graph.
+        saver = tf.train.import_meta_graph("D:/Document/my_model_final.ckpt.meta")  # this loads the graph structure
+        theta = tf.get_default_graph().get_tensor_by_name("theta:0")
+        with tf.Session() as sess:
+            saver.restore(sess, "D:/Document/my_model_final.ckpt")  # this restores the graph's state
+            best_theta_restored = theta.eval()
+            print(best_theta_restored)
+
+if __name__ == "__main__":
+    # run一个graph
+    # create_graph_and_run_in_session()
+    # graph管理
+    # graph_manage()
+    # 求解一个LR模型
+    # lr_train()
+    # 模型存储
+    save_load_model()
